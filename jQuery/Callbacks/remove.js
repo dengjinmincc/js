88fadbaed9f94cdaf95aa9e12fb1f5573ef40514
realMin <1436563611@qq.com>
2017-12-18 16:54:24 +0800

    callbacks.remove()

--- jQuery/Callbacks/remove.js
+++ jQuery/Callbacks/remove.js
@@ -1,18 +1,24 @@
 var callbacks = $.Callbacks();
 callbacks.add(test2);
 
+var val;
+
 function test1() {
-    if (callbacks.disabled()) {
+    // 如果回调列表不含有回调，即默认上一次回调并未完成，忽略用户操作
+    if (!callbacks.has(test2)) {
         return;
     }
+    val = $("#container select").val();
     console.log('test1');
     callbacks.fire();
 }
 
 function test2() {
-    callbacks.disable();
+    callbacks.remove(test2);
     setTimeout(function () {
-        console.log('test2');
+        console.log(val);
+        // 默认此次回调已经完成，用户可进行下一次操作
+        callbacks.add(test2);
     }, 5000);
 }
 
